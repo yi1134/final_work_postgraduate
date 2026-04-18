b5d781642be78d633f1d8465d3373591c3f50690
keybird <Djiacheng07@163.com>
2026-04-18 11:23:51 +0800

    初始建立项目

---
+++ src/main/java/com/keybird/edge_control_system/EdgeControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.keybird.edge_control_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeControlSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeControlSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keybird/edge_control_system/EdgeControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keybird.edge_control_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeControlSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
